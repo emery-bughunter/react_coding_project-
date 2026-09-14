34df8a43c2e7a92d5e6046febc28c458d3186415
AgertuDA <agertudiriba@gmail.com>
2026-09-14 10:56:34 +0200

    feat: add basic React TypeScript components

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+// Agertu Diriba
+
+import TeamDashboard from "./TeamDashboard";
+
+function App() {
+  return (
+    <div>
+      <h1>Team Dashboard</h1>
+      <p>Welcome to our team application.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/TeamDashboard.tsx
@@ -0,0 +1,12 @@
+//Agertu Diriba
+
+function TeamDashboard() {
+  return (
+    <div>
+      <h1>Team Dashboard</h1>
+      <p>This application helps our team manage members and tasks.</p>
+    </div>
+  );
+}
+
+export default TeamDashboard;
